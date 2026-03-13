658aecf92cb1dfaa46d91ce6c33178f5f1c25720
ssarathh12 <dil4u15@gmail.com>
2026-03-14 01:53:18 +0530

    font change try

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          orange: '#ED5906',
+          black: '#000000',
+          cream: '#F6EDDD',
+        },
+      },
+      fontFamily: {
+        // This is your 'funky' header font
+        display: ['Syne', 'sans-serif'],
+        // This is your 'clean' body font
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
-
